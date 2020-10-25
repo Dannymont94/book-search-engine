e8fb267694eccc7e7183feeb8d50593630f7b264
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-10-25 16:56:13 -0400

    fixed bug with apollo server context

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -29,6 +29,8 @@ module.exports = {
     } catch {
       console.log('Invalid token');
     }
+
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
